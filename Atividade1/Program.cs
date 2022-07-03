53371b226164e0b1e36cca03a28702bbb767c07a
Vinny Pereira Alves <vinny.alves004@hotmail.com>
2022-07-02 22:52:33 -0300

    Certo

--- Atividade1/Program.cs
+++ Atividade1/Program.cs
@@ -11,24 +11,48 @@ namespace Atividade1
         static void Main(string[] args)
         {
             string nome; 
-            int ntrabalho, n1, n2, media;
+            double ntrabalho, n1, n2, media;
 
             Console.WriteLine("Digite seu nome: ");
             nome = Console.ReadLine();
 
             Console.WriteLine("Digite a nota de trabalho: ");
-            ntrabalho = Convert.ToInt32(Console.ReadLine());
+            ntrabalho = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Digite a nota da primeira prova: ");
-            n1 = Convert.ToInt32(Console.ReadLine());
+            n1 = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Digite a nota da segunda prova: ");
-            n2 = Convert.ToInt32(Console.ReadLine());
+            n2 = double.Parse(Console.ReadLine());
 
             media = (ntrabalho + n1 + n2) / 3;
 
-            
-            
+            if (media >= 8.50 && media <= 10)
+            {
+                Console.WriteLine("Nota :" + media.ToString("n1"));
+                Console.WriteLine("Parabéns " + nome + ", você teve um rendimento final EXCELENTE!");
+            }
+            else if (media >= 7.0 && media >= 6.4)
+            {
+
+                Console.WriteLine("Nota :" + media.ToString("n1"));
+                Console.WriteLine("Parabens " + nome + ", você teve um BOM. Estude para ter um rendimento melhor!");
+            }
+            else if (media >= 6.0 && media <= 6.9)
+            {
+                Console.WriteLine("Nota :" + media.ToString("n1"));
+                Console.WriteLine(nome + ", você está aprovado, porém com uma nota razoavel, você pode melhorar!");
+            }
+            else if (media >= 0.1 && media <= 5.9)
+            {
+                Console.WriteLine("Nota :" + media.ToString("n1"));
+                Console.WriteLine(nome + ", Você está reprovado, estude mais para a proxima avalização!" );
+            }
+            else if (media == 0.00)
+            {
+                Console.WriteLine("Nota :" + media.ToString("n1"));
+                Console.WriteLine(nome + ", você foi reprovado, estude mais para a proxima avalização!");
+            }
 
             Console.ReadKey();
         }

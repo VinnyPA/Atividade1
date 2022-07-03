e6089e0589b28376ac23a2c26ea10daf86f3f89d
Vinny Pereira Alves <vinny.alves004@hotmail.com>
2022-07-02 21:48:05 -0300

    variantes

--- Atividade1/Program.cs
+++ Atividade1/Program.cs
@@ -10,6 +10,27 @@ namespace Atividade1
     {
         static void Main(string[] args)
         {
+            string nome; 
+            int ntrabalho, n1, n2, media;
+
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a nota de trabalho: ");
+            ntrabalho = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da primeira prova: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da segunda prova: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            media = (ntrabalho + n1 + n2) / 3;
+
+            
+
+
+            Console.ReadKey();
         }
     }
 }
